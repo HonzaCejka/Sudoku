658069a93321627b088a696117fa76fd21b2fbfd
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2022-10-19 20:43:28 +0200

    lol

--- Sudoku 0.1/Generate.cs
+++ Sudoku 0.1/Generate.cs
@@ -33,233 +33,25 @@ namespace Sudoku_0._1
         {
             for (int i = 0; i < 9; i++)
             {
-                switch (i)
-                {   case 0:
-                        for (int x = 0; x < 9; x++)
-                        {
+                for (int x = 0; x < 9; x++)
+                {
 
-                            int y = random.Next(1, 10);
-                            if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
+                    int y = random.Next(1, 10);
+                    if (Line.Contains(y))
+                    {
+                        board[i, x] = y;
+                        Line.Remove(y);
 
-                            }
-                            else
-                            {
-                                x--;
-                            }                            
-                        }
-                        SetLength(length);
-                        break;
-                    case 1:
-                        for (int x = 0; x < 9;x++)
-                        {                           
-                            int y = random.Next(1, 10);
-                            if (y == int.Parse(board.GetValue(i-1,x).ToString()))
-                            {
-                                x--;
-                            }
-                            else if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
-                                
-                            }
-                            else
-                            {
-                                x--;
-                            }
-                            
-                        }
-                        SetLength(length);
-                        break;
-                    case 2:
-                        for (int x = 0; x < 9; x++)
-                        {
-                            int y = random.Next(1, 10);
-                            if (y == int.Parse(board.GetValue(i - 1, x).ToString())|| y == int.Parse(board.GetValue(i - 2, x).ToString()))
-                            {
-                                x--;
-                            }
-                            else if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
-
-                            }
-                            else
-                            {
-                                x--;
-                            }
-
-                        }
-                        SetLength(length);
-                        break;
-                    case 3:
-                        for (int x = 0; x < 9; x++)
-                        {
-                            int y = random.Next(1, 10);
-                            if (y == int.Parse(board.GetValue(i - 1, x).ToString()) || y == int.Parse(board.GetValue(i - 2, x).ToString())|| y == int.Parse(board.GetValue(i - 3, x).ToString()))
-                            {
-                                x--;
-                            }
-                            else if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
-
-                            }
-                            else
-                            {
-                                x--;
-                            }
-
-                        }
-                        SetLength(length);
-                        break;
-                    case 4:
-                        for (int x = 0; x < 9; x++)
-                        {
-                            int y = random.Next(1, 10);
-                            if (y == int.Parse(board.GetValue(i - 1, x).ToString()) || y == int.Parse(board.GetValue(i - 2, x).ToString()) || y == int.Parse(board.GetValue(i - 3, x).ToString()) || y == int.Parse(board.GetValue(i - 4, x).ToString()))
-                            {
-                                x--;
-                            }
-                            else if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
-
-                            }
-                            else
-                            {
-                                x--;
-                            }
-
-                        }
-                        SetLength(length);
-                        break;
-                    case 5:
-                        for (int x = 0; x < 9; x++)
-                        {
-                            int y = random.Next(1, 10);
-                            if (y == int.Parse(board.GetValue(i - 1, x).ToString()) || y == int.Parse(board.GetValue(i - 2, x).ToString()) || y == int.Parse(board.GetValue(i - 3, x).ToString()) || y == int.Parse(board.GetValue(i - 4, x).ToString()) || y == int.Parse(board.GetValue(i - 5, x).ToString()))
-                            {
-                                x--;
-                            }
-                            else if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
-
-                            }
-                            else
-                            {
-                                x--;
-                            }
-
-                        }
-                        SetLength(length);
-                        break;
-                    case 6:
-                        for (int x = 0; x < 9; x++)
-                        {
-                            int y = random.Next(1, 10);
-                            if (y == int.Parse(board.GetValue(i - 1, x).ToString()) || y == int.Parse(board.GetValue(i - 2, x).ToString()) || y == int.Parse(board.GetValue(i - 3, x).ToString()) || y == int.Parse(board.GetValue(i - 4, x).ToString()) || y == int.Parse(board.GetValue(i - 5, x).ToString())|| y == int.Parse(board.GetValue(i - 6, x).ToString()))
-                            {
-                                x--;
-                            }
-                            else if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
-
-                            }
-                            else
-                            {
-                                x--;
-                            }
-
-                        }
-                        SetLength(length);
-                        break;
-                    case 7:
-                        for (int x = 0; x < 9; x++)
-                        {
-                            int y = random.Next(1, 10);
-                            if (y == int.Parse(board.GetValue(i - 1, x).ToString()) || y == int.Parse(board.GetValue(i - 2, x).ToString()) || y == int.Parse(board.GetValue(i - 3, x).ToString()) || y == int.Parse(board.GetValue(i - 4, x).ToString()) || y == int.Parse(board.GetValue(i - 5, x).ToString()) || y == int.Parse(board.GetValue(i - 6, x).ToString()) || y == int.Parse(board.GetValue(i - 7, x).ToString()))
-                            {
-                                x--;
-                            }
-                            else if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
-
-                            }
-                            else
-                            {
-                                x--;
-                            }
-
-                        }
-                        SetLength(length);
-                        break;
-                    case 8:
-                        for (int x = 0; x < 9; x++)
-                        {
-                            int y = random.Next(1, 10);
-                            if (y == int.Parse(board.GetValue(i - 1, x).ToString()) || y == int.Parse(board.GetValue(i - 2, x).ToString()) || y == int.Parse(board.GetValue(i - 3, x).ToString()) || y == int.Parse(board.GetValue(i - 4, x).ToString()) || y == int.Parse(board.GetValue(i - 5, x).ToString()) || y == int.Parse(board.GetValue(i - 6, x).ToString()) || y == int.Parse(board.GetValue(i - 7, x).ToString()) || y == int.Parse(board.GetValue(i - 8, x).ToString()))
-                            {
-                                x--;
-                            }
-                            else if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
-
-                            }
-                            else
-                            {
-                                x--;
-                            }
-
-                        }
-                        SetLength(length);
-                        break;
-                    case 9:
-                        for (int x = 0; x < 9; x++)
-                        {
-                            int y = random.Next(1, 10);
-                            if (y == int.Parse(board.GetValue(i - 1, x).ToString()) || y == int.Parse(board.GetValue(i - 2, x).ToString()) || y == int.Parse(board.GetValue(i - 3, x).ToString()) || y == int.Parse(board.GetValue(i - 4, x).ToString()) || y == int.Parse(board.GetValue(i - 5, x).ToString()) || y == int.Parse(board.GetValue(i - 6, x).ToString()) || y == int.Parse(board.GetValue(i - 7, x).ToString()) || y == int.Parse(board.GetValue(i - 8, x).ToString()) || y == int.Parse(board.GetValue(i - 9, x).ToString()))
-                            {
-                                x--;
-                            }
-                            else if (Line.Contains(y))
-                            {
-                                board[i, x] = y;
-                                Line.Remove(y);
-
-                            }
-                            else
-                            {
-                                x--;
-                            }
-
-                        }
-                        SetLength(length);
-                        break;
-                    default:
-                        break;
+                    }
+                    else
+                    {
+                        x--;
+                    }
                 }
-                
-                
+                SetLength(length);
+
             }
-            
-
         }
-
         public void ShowBoard()
         {
             for (int i = 0; i < 9; i++)
