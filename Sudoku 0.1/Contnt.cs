17228156f83b18334b67101f90768423da623037
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2022-11-23 13:18:05 +0100

    dif

--- Sudoku 0.1/Contnt.cs
+++ Sudoku 0.1/Contnt.cs
@@ -17,7 +17,7 @@ namespace Sudoku_0._1
         public Contnt(int dif)
         {
             con = "2;4;0;0;0;6;0;5;01;0;0;5;0;0;0;0;60;0;0;0;0;0;7;1;40;0;0;0;6;7;1;3;00;1;0;0;5;0;0;6;00;3;5;2;1;0;0;0;09;8;2;0;0;0;0;0;05;0;0;0;0;1;0;0;80;6;0;8;0;0;0;2;7";
-            Contall = new string[9, 9];
+            Contall = new string[dif, dif];
             Dif = dif;
             DoubleDifNaDruhou = Math.Pow(dif, 2);
         }
