3105d9b8ba24f3de060cf6f756fd869649b7fb74
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2022-11-28 12:17:30 +0100

    hura

--- Sudoku 0.1/MainWindow.xaml.cs
+++ Sudoku 0.1/MainWindow.xaml.cs
@@ -21,38 +21,140 @@ namespace Sudoku_0._1
     public partial class MainWindow : Window
     {
 
-
+        int Pick = 1;
+        int Dif = 9;
+        public TextBlock[,] PoleAll = new TextBlock[9,9];
+        public TextBlock[] Pole = new TextBlock[9];
+        public Rectangle border;
         
-        GenBoard bord = new GenBoard(9);
         Contnt cont = new Contnt(9);
         public MainWindow()
         {           
             InitializeComponent();            
             cont.build();
-            bord.gen();
-            bord.GridLines();
-            bord.PickerLines();
-            //GenContn();
-            
+            gen();   
+            GenContn();
+            picked();
         }
-
         
-        private void num1_MouseDown(object sender, MouseButtonEventArgs e)
+       
+        public void gen()
         {
-            bord.num1_MouseDown(sender, e);
+            for (int y = 0; y < Dif; y++)
+            {
+                for (int x = 0; x < Dif; x++)
+                {
+                    GridLines();
+                    PickerLines();
+                    Rectangle rect = new Rectangle();
+                    rect.Stroke = new SolidColorBrush(Colors.White);
+                    rect.StrokeThickness = 1;
+                    Pole[x] = new TextBlock();
+                    Pole[x].MouseDown += GridMouse;
+                    Pole[x].Foreground = Brushes.White;
+                    Pole[x].Width = 55;
+                    Pole[x].Height = 55;
+                    Pole[x].Tag = new int[2] { x, y };
+                    Pole[x].HorizontalAlignment = HorizontalAlignment.Center;
+                    Pole[x].VerticalAlignment = VerticalAlignment.Center;
+                    Pole[x].TextAlignment = TextAlignment.Center;
+                    Pole[x].FontSize = 36;
+                    Main.Children.Add(rect);
+                    Main.Children.Add(Pole[x]);
+                    Grid.SetColumn(rect, x);
+                    Grid.SetRow(rect, y);
+                    Grid.SetColumn(Pole[x], x);
+                    Grid.SetRow(Pole[x], y);
+                    PoleAll[x, y] = Pole[x];
+                }
+
+            }
         }
-        /*public void GenContn()
-         {
-             for (int y = 0; y < 9; y++)
-             {
-                 for (int x = 0; x < 9; x++)
-                 {
-                     PoleAll[y, x].Text = cont.Contall[x, y];                    
-                 }
-             }
+        public void GenContn()
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    PoleAll[y, x].Text = cont.Contall[x, y];
+                }
+            }
 
-         }*/
+        }
+        public void GridLines()
+        {
 
+            //opravit dif
+            Rectangle bigerRect = new Rectangle();
+            bigerRect.Stroke = new SolidColorBrush(Colors.White);
+            bigerRect.StrokeThickness = 3;
+            Main.Children.Add(bigerRect);
+            Grid.SetColumn(bigerRect, 0);
+            Grid.SetRow(bigerRect, 0);
+            Grid.SetColumnSpan(bigerRect, Dif);
+            Grid.SetRowSpan(bigerRect, Dif);
+
+            for (int r = 0; r < Dif; r += 3)
+            {
+                for (int i = 0; i < Dif; i += 3)
+                {
+                    Rectangle bigRect = new Rectangle();
+                    bigRect.Stroke = new SolidColorBrush(Colors.White);
+                    bigRect.StrokeThickness = 2;
+                    Main.Children.Add(bigRect);
+                    Grid.SetColumn(bigRect, i);
+                    Grid.SetRow(bigRect, r);
+                    Grid.SetColumnSpan(bigRect, 3);
+                    Grid.SetRowSpan(bigRect, 3);
+                }
+            }
+        }
+        public void num1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            TextBlock tb = sender as TextBlock;
+            Pick = int.Parse(tb.Tag.ToString());
+            picked();
+
+        }
+
+        public void GridMouse(object sender, MouseButtonEventArgs e)
+        {
+
+            TextBlock ceman = sender as TextBlock;
+            int[] exos = ceman.Tag as int[];
+            int x = exos[0];
+            int y = exos[1];
+            if (cont.Contall[y, x] == " ")
+            {
+                PoleAll[x, y].Text = Pick.ToString();
+            }
+            else
+            {
+
+            }
+        }
+
+        public void PickerLines()
+        {
+            for (int i = 0; i < Dif; i++)
+            {
+                border = new Rectangle();
+                border.Stroke = new SolidColorBrush(Colors.White);
+                Picker.Children.Add(border);
+                Grid.SetColumn(border, i);
+                Grid.SetRow(border, i);
+            }
+        }
+        public void picked()
+        {
+            border.Stroke = new SolidColorBrush(Colors.Aqua);
+            border.Fill = new SolidColorBrush(Colors.Aqua);
+            border.StrokeThickness = 2;
+            Grid.SetZIndex(border, -1);
+            Grid.SetColumn(border, Pick - 1);
+            Grid.SetRow(border, Pick - 1);
+        }
 
 
 
